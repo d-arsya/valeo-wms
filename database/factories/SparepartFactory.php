4dfdc333d9a2160435e995ad7645e55b37a338d2
d-arsya <kamaluddin.arsyad17@gmail.com>
2026-05-12 11:13:33 +0700

    docs: Plan

--- database/factories/SparepartFactory.php
+++ database/factories/SparepartFactory.php
@@ -30,8 +30,7 @@ class SparepartFactory extends Factory
             'last_supplier' => $this->faker->company(),
             'last_gr_date' => $this->faker->date(),
             'price_per_unit' => $this->faker->randomFloat(2, 10, 1000),
-            'status' => 'OK',
-            'qr_code_path' => null,
+            'status' => 'OK'
         ];
     }
 }
